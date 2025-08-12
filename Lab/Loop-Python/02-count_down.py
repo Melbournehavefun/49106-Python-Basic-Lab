da3e1115c6ee351673e80985b2d0645ef4b82517
Melbourne <mr.melbourn2@gmail.com>
2025-08-12 18:41:17 +0700

    ใบงานที่ 6 แบบฝึกปฏิบัติ Python เรื่อง for loop 1

---
+++ Lab/Loop-Python/02-count_down.py
@@ -0,0 +1,6 @@
+num = int(input("Enter number: "))
+i = num
+for count in range(1,num+1):
+    print(i)
+    i = i-1
+    count = count + 1
---
+++ Lab/Loop-Python/03-times_table.py
@@ -0,0 +1,8 @@
+num = int(input("Enter Number: "))
+
+for i in range(1,13):
+    print(f"{num} x {i} = {num * i}")
+    i = i + 1
+
+
+    
